fd3636290167a975f1e185ffcf5d4111122373ab
gab-r-x <ryksdev@gmail.com>
2024-10-15 18:35:15 -0300

    flask initial configuration

---
+++ app/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import json
+
+from app.extensions import configuration
+
+
+def minimal_app(**config):
+    app = Flask(__name__)
+    configuration.init_app(app, **config)
+    return app
+
+
+def create_app(**config):
+    app = minimal_app(**config)
+    configuration.load_extensions(app)
+    
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+    return app
---
+++ app/extensions/configuration.py
@@ -0,0 +1,15 @@
+from importlib import import_module
+from dynaconf import FlaskDynaconf
+
+def load_extensions(app):
+    for extension in app.config.EXTENSIONS:
+        # Split data in form `extension.path:factory_function`
+        module_name, factory = extension.split(":")
+        # Dynamically import extension module.
+        extension = import_module(module_name)
+        # Invoke factory passing app.
+        getattr(extension, factory)(app)
+
+
+def init_app(app, **config):
+    FlaskDynaconf(app, **config)
